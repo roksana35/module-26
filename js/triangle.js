a23d9ccbadd58d26758acf39b111f4755268ad03
roksana35 <Roksanaakter7235@gmail.com>
2024-02-13 17:02:49 +0600

    first commit

---
+++ js/triangle.js
@@ -0,0 +1,18 @@
+function triangle (){
+    // triangle base added
+    const triangletext =document.getElementById('triangle-base');
+
+    const trianglebase =  triangletext.value;
+    const persflorts= parseFloat(trianglebase);
+    // console.log( persflorts);
+
+    // triangle  height added
+    const triangletext2 = document.getElementById('triangle-height');
+    const triangleheight = triangletext2.value;
+    const persflorts2 = parseFloat(triangleheight);
+    // console.log(persflorts2);
+    // calculate
+    const area = 0.5*persflorts*persflorts2;
+    console.log(area);
+
+}
